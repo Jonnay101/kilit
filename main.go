6c9f913254f23ffc8db19040155aa5c0d0038900
John Hughes <john.hughes101@gmail.com>
2020-01-30 15:01:03 +0000

    initial code

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+)
 
+func main() {
+	port := flag.String("p", "8080", "the port you want to kill")
+	flag.Parse()
+	task := "lsof"
+	cmd := exec.Command(task, "-i", fmt.Sprintf(":%s", *port))
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(bytes.NewReader(out))
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "COMMAND") {
+			continue
+		}
+		if line == "" {
+			break
+		}
+		words := strings.Fields(line)
+		pid := words[1]
+		kill := exec.Command("kill", "-9", pid)
+		kill.Run()
+	}
 }
