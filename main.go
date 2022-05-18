036b7b58faa899963efe0aa70243b5999f9e7abb
John Hughes <john.hughes@musictribe.com>
2022-05-18 08:46:55 +0100

    feat: :sparkles: Use main.go as a constuctor to put all the peices together and execute the package

--- main.go
+++ main.go
@@ -1,37 +1,25 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"flag"
-	"fmt"
-	"log"
-	"os/exec"
-	"strings"
+
+	"github.com/jonnay101/kilit/command"
+	"github.com/jonnay101/kilit/domain"
 )
 
 func main() {
 	port := flag.String("p", "8080", "the port you want to kill")
 	flag.Parse()
-	task := "lsof"
-	cmd := exec.Command(task, "-i", fmt.Sprintf(":%s", *port))
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Fatal(err)
-	}
-	scanner := bufio.NewScanner(bytes.NewReader(out))
-	scanner.Split(bufio.ScanLines)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.Contains(line, "COMMAND") {
-			continue
-		}
-		if line == "" {
-			break
-		}
-		words := strings.Fields(line)
-		pid := words[1]
-		kill := exec.Command("kill", "-9", pid)
-		kill.Run()
-	}
+
+	Do("lsof", "-i", *port)
+}
+
+func Do(name string, params ...string) {
+	store := new(domain.DataStore)
+	command.CreateData(store, name, params...)
+
+	killer := new(command.ProcessKiller)
+	domain.GetAllPIDsFromData(store.Data(), killer)
+
+	killer.Kill()
 }
